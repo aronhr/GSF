bd30e650ac20e7dce281c54de7058f004d2f71d5
Aron Hrafnsson <aronhr98@gmail.com>
2017-12-15 01:48:30 +0000

    Lokaverkefni, sett ath hvort nemandi hafi fallið á önn og að áfangar sem nemandi hefur fallið í koma aftur í val

--- Verkefni_5/Lokaverkefni.sql
+++ Verkefni_5/Lokaverkefni.sql
@@ -1,3 +1,15 @@
+/*
+Skrifið Stored Procedure EN: ElectedCourses / IS: MittVal.
+
+Kalla skal á þennan SP með nemendanúmeri og hann skilar til baka vali fyrir næstu önn.
+
+[x] 1:  Það má alveg takmarka fjölda áfnaga við t.d. 5 eða einhvern max einingafjölda/feiningafjölda.
+[x] 2:  SP þarf að finna þessa áfanga útfrá óloknum áföngum. Skoða þarf hvaða áfangar koma næst sé það hægt.
+[x] 3:  Ekki þarf að kanna fall í núverandi áföngum(þeir áfangar sem nemandinn er í en hefur ekki klárað þegar val fer fram).
+[x] 4:  Ef allt klikkar þá velur SP random úr þeim áföngum sem nemandinn á eftir að taka.
+*/
+
+
 use 0712982139_progresstracker_v1;
 
 DROP PROCEDURE IF EXISTS ElectedCourses;
@@ -5,16 +17,34 @@ DROP PROCEDURE IF EXISTS ElectedCourses;
 DELIMITER $$
 
 CREATE PROCEDURE ElectedCourses(IN s_studentID INT(11))
-
 BEGIN
+  DECLARE counting INT;
+  DECLARE limit_q INT;
+
+  SET counting = (SELECT count(courseNumber) FROM studentcourses WHERE studentcourses.studentID = s_studentID);
+
+  IF(counting >= 1) THEN
+	SET limit_q = 5;
+    END IF;
+
+  IF(counting = 0) THEN
+    set limit_q = 5;
+      END IF;
+
 	SELECT courseNumber, courseName, courseCredits
      FROM courses
        WHERE NOT EXISTS
          (SELECT courseNumber
            FROM studentcourses
-             WHERE studentcourses.courseNumber = courses.courseNumber AND studentcourses.studentID = s_studentID )ORDER BY RAND() LIMIT 5 ;
+             WHERE studentcourses.courseNumber = courses.courseNumber AND studentcourses.studentID = s_studentID )ORDER BY RAND() LIMIT limit_q ;
+
+
 END $$
 
 DELIMITER ;
 
+<<<<<<< HEAD
+CALL ElectedCourses(0712980139);
+=======
 CALL ElectedCourses(0712982139);
+>>>>>>> 765502f9ea61041fb8f88a13020c3f5d04a9a972
